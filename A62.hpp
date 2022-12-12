ca16f2ce180ffe7083f412c39459988128868646
Kyu Lee <kwooler@gmail.com>
2022-12-12 10:29:07 -0800

    A62

--- A62.hpp
+++ A62.hpp
@@ -8,35 +8,14 @@ double nthpowerp(int);
 double nthpowern(int);
 void printout(double);
 
-// // complete two functions
-// double nthpowerp(int n)
-// {
-//         // return n_th power number
-// }
-// double nthpowern(int n)
-// {
-//         // return n_th power number
-// }
-// void printout(double result)
-// {
-//         cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
-// }
+// complete two functions
 double nthpowerp(int n)
 {
-        int i;
-        double pow = 1.0;
-        for (i = 0; i < n; i++)
-                pow *= 2;
-        return pow;
+        // return n_th power number
 }
 double nthpowern(int n)
 {
-        int i;
-        double pow = 1.0;
-        for (i = n; i < 0; i++)
-                pow *= 2;
-        pow = 1.0 / pow;
-        return pow;
+        // return n_th power number
 }
 void printout(double result)
 {
