63116e656672945273f9e83eeb7dbf0ab4f46de1
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-15 17:43:08 -0700

    Lab 6-2

--- main.hpp
+++ main.hpp
@@ -13,13 +13,31 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        static bool srand_called = false;
+
+        if (!srand_called) {
+        srand(time(nullptr));
+        srand_called = true;
+        }
+        return rand() % 100;
 }
+
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
+
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        int mean = static_cast<double>(rdnum1 + rdnum2 + rdnum3) / 3;
+
+        int diff1 = abs(rdnum1 - mean);
+        int diff2 = abs(rdnum2 - mean);
+        int diff3 = abs(rdnum3 - mean);
+
+        int minDiff = min(diff1, min(diff2, diff3));
+
+        if (diff1 == minDiff) {
+                return rdnum1;
+        } else if (diff2 == minDiff) {
+                return rdnum2;
+        } else {
+                return rdnum3;
+        }
 }
